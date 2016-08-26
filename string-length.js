c1d451380937b05700cbdac5ac9f602996dcd024
kopiusmaximus <alexander.kopynec@gmail.com>
2016-08-26 16:46:37 -0400

    Complete exercise 4
    Create file string-length.js
    Create a string variable and print its length to the console.

---
+++ string-length.js
@@ -0,0 +1,15 @@
+// Create a file named string-length.js.
+//
+// In that file, create a variable named example.
+//
+// Assign the string 'example string' to the variable example.
+//
+// Use console.log to print the length of the string to the terminal.
+//
+// Check to see if your program is correct by running this command:
+
+'use strict';
+
+let example = 'example string';
+
+console.log(example.length);
