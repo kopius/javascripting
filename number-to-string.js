62680f4ebb320d441ef7248e13af8c9cbebe6ca7
kopiusmaximus <alexander.kopynec@gmail.com>
2016-08-27 13:10:19 -0400

    Complete exercise 8
    Create file number-to-string.js
    Create a number variable and convert it to a string using .toString().

---
+++ number-to-string.js
@@ -0,0 +1,16 @@
+// Create a file named number-to-string.js.
+//
+// In that file define a variable named n that references the number 128;
+//
+// Call the .toString() method on the n variable.
+//
+// Use console.log() to print the results of the .toString() method to the
+// terminal.
+
+'use strict';
+
+let n = 128;
+
+n = n.toString();
+
+console.log(n);
