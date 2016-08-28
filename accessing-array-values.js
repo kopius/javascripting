0adc0b35fc92957f83d90fc2cef0a1771494f701
kopiusmaximus <alexander.kopynec@gmail.com>
2016-08-28 12:02:31 -0400

    Complete exercise 13
    Create an array and print its second value to the console.

---
+++ accessing-array-values.js
@@ -0,0 +1,13 @@
+// Create a file named accessing-array-values.js.
+//
+// In that file, define array food :
+//
+//    var food = ['apple', 'pizza', 'pear'];
+//
+// Use console.log() to print the second value of array to the terminal.
+
+'use strict';
+
+let food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
